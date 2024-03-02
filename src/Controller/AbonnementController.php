fc919e03850b76fb07c4ed0904e7e7b4e8227e65
Altergo-01 <axelbreheret@gmail.com>
2024-03-02 20:58:20 +0100

    feat: UI sub

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Subscription;
+use App\Repository\PdfRepository;
+use App\Repository\SubscriptionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,8 +23,12 @@ class AbonnementController extends AbstractController
     }
 
     #[Route('/abonnement', name: 'app_abonnement')]
-    public function index(): Response
-    {
+    public function index(
+        SubscriptionRepository $subscriptionRepository,
+        EntityManagerInterface $entityManager,
+        Request $request,
+        PdfRepository $pdfRepository
+    ): Response {
         $user = $this->getUser();
         $subscriptions = $this->entityManager->getRepository(Subscription::class)->findAll();
         return $this->render('abonnement/index.html.twig', [
